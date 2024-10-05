b4335473e74b9afdddb9276ee4581ea60b6b41a8
enigmak9 <enigmak9@protonmail.com>
2024-10-04 19:06:08 -0600

    second commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstAssignment
+//
+//  Created by Carlos Ignacio Padilla Herrera on 04/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
